0601d77d69f2206a02f33fe520962b4a97e8114b
abdelrhman ahmed kamal <abdelrhman.ahmed.nassar@gmail.com>
2022-06-25 18:37:31 +0200

    Updates

---
+++ static/js/922.b301db46.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexpenses_app=self.webpackChunkexpenses_app||[]).push([[922],{922:function(e,n,s){s.r(n);var p=s(184);n.default=function(){return(0,p.jsx)("div",{className:"center",children:(0,p.jsx)("p",{children:"Page not found!"})})}}}]);
+//# sourceMappingURL=922.b301db46.chunk.js.map
